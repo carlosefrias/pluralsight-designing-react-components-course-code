4cee4072bd334cb13f90c59ce8f4d17181eaefc8
Carlos Frias <carlosefrias@gmail.com>
2020-09-12 17:50:55 +0100

    initial commit

---
+++ react-components-cf/pages/speakers.js
@@ -0,0 +1,18 @@
+import Header from '../src/components/Header/Header'
+import Footer from '../src/components/Footer/Footer'
+import Menu from '../src/components/Menu/Menu'
+import Speakers from '../src/components/Speakers/Speakers'
+import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBar'
+
+function Page(){
+    return (
+        <div>
+            <Header />
+            <Menu />
+            <SpeakerSearchBar />
+            <Speakers />
+            <Footer />
+        </div>
+    )
+}
+export default Page
---
+++ react-components-cf/src/components/SpeakerSearchBar/SpeakerSearchBar.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const SpeakerSearchBar = () => <img src="images/searchbar.png" />
+
+export default SpeakerSearchBar
